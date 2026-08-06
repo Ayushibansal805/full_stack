9a97f732102d1686c8d2c6b8f8cc77c92889f71c
Ayushi Bansal <ayushi.bansal_cs.h24@gla.ac.in>
2026-08-06 14:19:05 +0530

    light dark toggle mode

---
+++ script.js
@@ -0,0 +1,33 @@
+// var data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// for (let i=0; i<data.length; i++){
+//     console.log(data[i]**2);
+// }
+
+// functions
+
+// var fn = () => {
+//     console.log("hello");
+// }
+
+// var fn = (a, b) => {
+//     console.log(a+b);
+// }
+
+// fn(2, 5);
+
+// const product = function(x, y){
+//     console.log(x*y);
+// }
+// product(2, 5);
+
+// var fn = (data) => {
+//     for (let i=0; i<data.length; i++){
+//         console.log(data[i]);
+//     }
+// }
+// var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// fn(arr);
+
+
+// DOM Manipulation
